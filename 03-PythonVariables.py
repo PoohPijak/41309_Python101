eea7d9ab8c78f792a4696ec9d9e37f891cf5069d
PoohPijak <115363349+PoohPijak@users.noreply.github.com>
2026-06-24 10:02:53 +0700

    Create 03-PythonVariables.py

---
+++ 03-PythonVariables.py
@@ -0,0 +1,9 @@
+# 1. Assign values to the variables as specified (ข้อมูลนำเข้า)
+team1 = "Liverpool"
+team2 = "Chelsea"
+score1 = 4
+score2 = 3
+
+# 2. Print the output in 2 lines as required (ข้อมูลส่งออก)
+print(f"{team1} versus {team2}")
+print(f"Final score: {score1} to {score2}")
